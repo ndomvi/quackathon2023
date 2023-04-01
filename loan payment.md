a8e2f8b1ef6baca533d588866e6de312d681723b
Jmomanyi <jjjmomanyi@gmail.com>
2023-04-01 19:13:06 +0100

    first commit

---
+++ loan payment.md
@@ -0,0 +1,7 @@
+# How to pay off loans
+## Paying the minimum payments on all loans
+ You should always prioritise paying the minimum payments, if there are any. This should be done before paying off other debt.  
+ Not paying minimum payments will incur late fees, which will make you owe more money.
+## Highest rate first
+  When paying debt it is importatn to prioritise debts with the highest interest rate, no matter how big or small it is.  
+  The debts with high interest rates are accruing the most interest over time. By paying them off early, you save money in the long term.
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const daisyui = require('daisyui');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./web/**/*.html",
+    "./src/**/*.js",
+  ],
+
+  daisyui: {
+    themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
+
